f1bfedfbd744274089a860340402e453196f9b55
vision800 <netshokotshokovision112@gmail.com>
2024-01-10 10:02:41 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sayHi();
+            sayHiUser("Vision");
+            sayHiAge("Vision", 23);
+        }
+
+        static void sayHi()
+        {
+            Console.WriteLine("Hi");
+        }
+
+        static void sayHiUser(string name) {
+            Console.WriteLine("Hi, "+name);
+        }
+
+        static void sayHiAge(string name, int age)
+        {
+            Console.WriteLine("Hi, " + name+" your age is "+age);
+        }
+
+    }
+}
